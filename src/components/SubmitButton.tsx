691f7d78ad786dfa0ab1ba784eb8b68a3a2db3b6
Harry Best <harrybest800@gmail.com>
2021-12-11 17:28:36 +0000

    Add input click event

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import { Current_Line, Foreground, Purple, Red, Yellow } from "../colours";
 
 export const SubmitButton = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const StyledButton = styled.button`
     height: calc(100% + 32px);
     width: calc(100% + 32px);
@@ -16,7 +18,16 @@ export const SubmitButton = () => {
     text-decoration: none;
   `;
 
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  }
+
   return (
-    <StyledButton>I await your dish...</StyledButton>
+    <>
+      <input ref={inputRef} type="file" hidden/>
+      <StyledButton onClick={() => handleClick()}>I await your dish...</StyledButton>
+    </>
   )
 }
