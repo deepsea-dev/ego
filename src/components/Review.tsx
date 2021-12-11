1b7107952c7d9f9260e8bd682b000aa9172e2903
Harry Best <harrybest800@gmail.com>
2021-12-11 21:35:39 +0000

    Change review breakpoint

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { Card } from "./Card";
 import { Emph } from './Emph';
 
+const mediaBreakpoint = '850px';
+
 const egoReviewArray = `In many ways, the work of a critic is easy. We risk very little yet enjoy a position over those who offer up their work and their selves to our judgment. We thrive on negative criticism, which is fun to write and to read. But the bitter truth we critics must face, is that in the grand scheme of things, the average piece of junk is probably more meaningful than our criticism designating it so.\n But there are times when a critic truly risks something, and that is in the discovery and defense of the new. The world is often unkind to new talent, new creations. The new needs friends.
 
 Last night, I experienced something new, %s from a singularly unexpected source. To say that both the meal and its maker have challenged my preconceptions about fine cooking is a gross understatement. They have rocked me to my core. In the past, I have made no secret of my disdain for Chef Gusteau's famous motto: 'Anyone can cook.' But I realize, only now do I truly understand what he meant. Not everyone can become a great artist, but a great artist can come from anywhere. It is difficult to imagine more humble origins than those of the genius now cooking at Gusteau's, who is, in this critic's opinion, nothing less than the finest chef in France. I will be returning to Gusteau's soon, hungry for more.`.split(/%s/g)
@@ -53,7 +55,7 @@ export const Review: React.FC<ReviewProps> = ({imageUrl}) => {
     display: flex;
     column-gap: 32px;
     
-    @media (max-width: 800px) {
+    @media (max-width: ${mediaBreakpoint}) {
       flex-direction: column;
       width: 90%;
       row-gap: 32px;
@@ -68,7 +70,7 @@ export const Review: React.FC<ReviewProps> = ({imageUrl}) => {
   const FoodCard = styled(Card)`
     width: 30%;
 
-    @media (max-width: 800px) {
+    @media (max-width: ${mediaBreakpoint}) {
       width: 100%;
     }
   `
@@ -76,7 +78,7 @@ export const Review: React.FC<ReviewProps> = ({imageUrl}) => {
   const ReviewCard = styled(Card)`
     width: 70%;
 
-    @media (max-width: 800px) {
+    @media (max-width: ${mediaBreakpoint}) {
       width: 100%;
     }
   `
