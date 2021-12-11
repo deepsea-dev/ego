75957c0ba2f343f1cf5154d96f827534005cd8c1
Harry Best <harrybest800@gmail.com>
2021-12-11 15:54:22 +0000

    Add image of ego

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import { Background } from './colours';
 import { Header } from './components/Header';
+import Ego from './images/Ego.webp';
+
 function App() {
   return (
-    <Body>
-      <Header/>
-    </Body>
+  <>
+    <Header/>
+    <Wrapper>
+      <Body>
+      </Body>  
+      <EgoImage src={Ego} alt={"Anton Ego"} />
+    </Wrapper>
+  </>
   );
 }
 
 export default App;
 
+const Wrapper = styled.div`
+  padding-top: 120px;
+  margin-left: 50px;
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+  overflow-x: hidden;
+`;
+
 const Body = styled.div`
-  background-color: ${Background};
-  height: 100vh;
-  width: 100vw;
-`
+  width: 80% !important;
+  min-width: 200px;
+`;
+
+const EgoImage = styled.img`
+  height: 85vh;
+`;
