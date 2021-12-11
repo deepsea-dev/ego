8443576f79b830d129cb8224d4ef752f0f12f3bd
Harry Best <harrybest800@gmail.com>
2021-12-11 15:25:57 +0000

    Add topbar and background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Background } from './colours';
 import { Header } from './components/Header';
 function App() {
   return (
-    <Header/>
+    <Body>
+      <Header/>
+    </Body>
   );
 }
 
 export default App;
+
+const Body = styled.div`
+  background-color: ${Background};
+  height: 100vh;
+  width: 100vw;
+`
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,22 @@
-import { Box, Typography } from '@mui/material';
+import { AppBar, Box, Typography } from '@mui/material';
 import React from 'react';
 import styled from 'styled-components';
+import { Current_Line, Selection } from '../colours';
 
 export const Header = () => (
   <HeaderBox>
-    <Typography variant="h1" component="div">Anton Ego</Typography>
+    <HeaderBar>
+      <Typography variant="h3" component="div">Anton Ego</Typography>
+    </HeaderBar>
   </HeaderBox>
 )
 
 const HeaderBox = styled(Box)`
   display: flex;
   flex-grox: 1;
+`
+const HeaderBar = styled(AppBar)`
+  padding: 16px;
+  background-color: ${Current_Line};
+  text-colour: ${Selection};
 `
